cd577089c00012bddb926beee50be877b7340be4
oyama <oyama@ilovex.co.jp>
2020-10-19 17:08:37 +0900

    Adopt React.js(Create React App)

---
+++ build/precache-manifest.a0ada59467783641ceadaaf52b7ecc61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81a218e3c4ccdafa647570ae1584285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd41ca29c71bf7114f60",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "012add02761e74194e44",
+    "url": "/static/js/2.21523c7b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.21523c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd41ca29c71bf7114f60",
+    "url": "/static/js/main.d27c6b85.chunk.js"
+  },
+  {
+    "revision": "af0fc021298cd1e4dc91",
+    "url": "/static/js/runtime-main.0c62b6cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
